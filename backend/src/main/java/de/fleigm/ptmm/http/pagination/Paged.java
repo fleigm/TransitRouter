f764ac457ce2b7b203f70603067c4eaefeefc3bf
Michael Fleig <michifleig@gmail.com>
2020-11-27 17:16:47 +0100

    remove final fields so it can be used as @BeanParam

--- backend/src/main/java/de/fleigm/ptmm/http/pagination/Paged.java
+++ backend/src/main/java/de/fleigm/ptmm/http/pagination/Paged.java
@@ -1,23 +1,27 @@
 package de.fleigm.ptmm.http.pagination;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Min;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.QueryParam;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Paged {
 
   @QueryParam("page")
   @DefaultValue("1")
   @Min(value = 0, message = "Page must be positive.")
-  private final int page;
+  private int page;
 
   @QueryParam("limit")
   @DefaultValue("30")
   @Min(value = 10, message = "Limit must be at least 10.")
-  private final int limit;
+  private int limit;
 
   public static Paged of(int page, int limit) {
     return new Paged(page, limit);
