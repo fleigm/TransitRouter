d54dac1910eb16d7535dc23c5eb3ac2d0e0dc4a8
Michael Fleig <michifleig@gmail.com>
2020-11-30 16:56:33 +0100

    update assets

--- frontend/design/tailwind.config.js
+++ frontend/design/tailwind.config.js
@@ -1,7 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    purge: [],
+    purge: [
+        './src/**/*.html',
+        './src/**/*.vue',
+        './src/**/*.jsx',
+    ],
     theme: {
         extend: {
             height: {
