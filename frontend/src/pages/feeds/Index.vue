8afde71b808e833316df52ec57d2b04960dd4cc0
Michael Fleig <michifleig@gmail.com>
2021-02-08 17:07:12 +0100

    page transitions

--- frontend/src/pages/feeds/Index.vue
+++ frontend/src/pages/feeds/Index.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="flex-1">
-<!--
-    <div class="container flex justify-between items-center">
-      <v-breadcrumbs></v-breadcrumbs>
-      <div id="context-menu">
-      </div>
-    </div>
--->
+    <!--
+        <div class="container flex justify-between items-center">
+          <v-breadcrumbs></v-breadcrumbs>
+          <div id="context-menu">
+          </div>
+        </div>
+    -->
 
-    <router-view></router-view>
+    <transition enter-active-class="transition-all transition-opacity duration-150"
+                enter-from-class="opacity-0"
+                enter-to-class="opacity-100"
+                leave-active-class="transition-all transition-opacity duration-150"
+                leave-from-class="opacity-100"
+                leave-to-class="opacity-0">
+      <router-view class="bg-white"></router-view>
+    </transition>
   </div>
 </template>
 
@@ -19,7 +26,7 @@ export default {
   name: "Index",
 
   mounted() {
-    Breadcrumbs.set({name: 'Feeds', route: { name: 'presets.index'}})
+    Breadcrumbs.set({name: 'Feeds', route: {name: 'presets.index'}})
   }
 }
 </script>
